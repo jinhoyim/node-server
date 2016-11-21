5aceaf89c7dd641cd94dc9eb75e67fd6e2b3a95d
Jinho Yim <jinhoyim@outlook.com>
2016-11-21 16:29:56 +0900

    added route home

--- first-node-server/app.js
+++ first-node-server/app.js
@@ -1,9 +1,12 @@
 var http = require('http');
 
 http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type':'text/plain'});
-  response.write('First Node Application');
-  response.end();
+  console.log(request.url);
+
+  if(request.url === '/'){
+    response.write('This is home.');
+    response.end();
+  }
 }).listen(process.env.PORT || 3030);
 
 console.log('First Node Server');
