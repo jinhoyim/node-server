5b5c5ff64b7055b664d34b9751d77facd600204c
Jinho Yim <jinhoyim@outlook.com>
2016-11-21 17:49:30 +0900

    added route.js

--- first-node-server/app.js
+++ first-node-server/app.js
@@ -1,12 +1,13 @@
 var http = require('http');
+var router = require('./router');
 
 http.createServer(function(request, response){
   console.log(request.url);
 
-  if(request.url === '/'){
-    response.write('This is home.');
-    response.end();
-  }
+  router.home(request, response);
+  router.about(request, response);
+  router.fastcampus(request, response);
+
 }).listen(process.env.PORT || 3030);
 
 console.log('First Node Server');
