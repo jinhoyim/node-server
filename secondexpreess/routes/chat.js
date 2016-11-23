3abe68892c9745c3081e2ed279c385e2247a89a7
Jinho Yim <jinhoyim@outlook.com>
2016-11-24 02:12:45 +0900

    fixed chat

--- secondexpreess/routes/chat.js
+++ secondexpreess/routes/chat.js
@@ -1,32 +1,15 @@
 module.exports = function(app){
-  var http = require('http').Server(app);
-  var io = require('socket.io')(http);
+  // var http = require('http').Server(app);
+  // var io = require('socket.io')(http);
 
   // http.listen(80);
-  app.get('/chat', function(req, res){
-    res.render('chat.ejs');
-  });
-
-  io.on('connection', function(socket){
-    console.log('new user is connected.');
-
-    socket.on('chat message', function(msg){
-     console.log("message: " + msg);
-    });
-
-    socket.on('disconnect', function () {
-      console.log('user disconnected');
-      io.emit('user disconnected');
-    });
-  });
-// socket.emit("chat message", msg);
-//     socket.on("chat message", function(msg){
-//       console.log("message: " + msg);
-//     });
+app.get('/chat', function(req, res){
+  res.render('chat.ejs');
+});
   
 
-  http.listen(3000, function(){
-    console.log('Chat server is listen on 3000');
-  });
+  // http.listen(3000, function(){
+  //   console.log('Chat server is listen on 3000');
+  // });
 
 }
--- secondexpreess/server.js
+++ secondexpreess/server.js
@@ -5,6 +5,31 @@ var aboutRouter = require('./routes/about')(app);
 var usersRouter = require('./routes/users')(app);
 var charRouter = require('./routes/chat')(app);
 
+var http = require('http');
+
+// var server = http.Server(app);
+// var io = require('socket.io')(server);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+// io.emit('some event', { for: 'everyone' });
+io.on('connection', function(socket){
+  console.log('new user is connected.');
+  
+  socket.broadcast.emit('chat message', 'new user joined');
+
+  socket.on('chat message', function(msg){
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    io.emit('user disconnected');
+    socket.broadcast.emit('chat message', 'user left');
+  });
+});
+
 app.set('port', process.env.PORT | 3030);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -27,6 +52,10 @@ app.use(function(req, res){
   res.send('500 - Server Error');
 });
 
-app.listen(app.get('port'), function(){
+// app.listen(app.get('port'), function(){
+//   console.log('Second Express server is running at localhost:' + app.get('port'));
+// });
+
+server.listen(app.get('port'), function(){
   console.log('Second Express server is running at localhost:' + app.get('port'));
 });
